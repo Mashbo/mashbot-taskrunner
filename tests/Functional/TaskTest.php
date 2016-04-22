4d64db7a9af588aef35044a1b3e663dd17cfbcbd
Adam Quaile <aquaile@mashbo.com>
2016-04-22 11:10:43 +0100

    Added base TaskTest to help with testing tasks

---
+++ tests/Functional/TaskTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Mashbo\Mashbot\Tests\Functional;
+
+use Mashbo\Mashbot\TaskRunner\Invocation\TaskInvoker;
+use Mashbo\Mashbot\TaskRunner\TaskContext;
+use Mashbo\Mashbot\TaskRunner\TaskRunner;
+use Prophecy\Argument;
+use Prophecy\Prophecy\MethodProphecy;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophet;
+use Prophecy\Util\StringUtil;
+use Psr\Log\NullLogger;
+
+abstract class TaskTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var callable
+     */
+    private $sut;
+
+    /**
+     * @var TaskRunner|ObjectProphecy
+     */
+    protected $runner;
+
+    /**
+     * @return callable
+     */
+    abstract protected function getTask();
+
+
+    public function setUp()
+    {
+        $prophet = new Prophet();
+        $this->sut = $this->getTask();
+        $this->runner = $prophet->prophesize(TaskRunner::class);
+        $this->runner->invoke(Argument::cetera())->will(function($args, ObjectProphecy $object, MethodProphecy $method) {
+
+            $expected  = implode("\n", array_map(function (MethodProphecy $methodProphecy) {
+                return sprintf('  - %s(%s)',
+                    $methodProphecy->getMethodName(),
+                    $methodProphecy->getArgumentsWildcard()
+                );
+            }, call_user_func_array('array_merge', $object->getMethodProphecies())));
+
+            throw new \LogicException("{$method->getMethodName()} with " . (new StringUtil())->stringify($args) . " was not expected, expected calls were: \n" . $expected);
+        });
+    }
+
+    protected function invoke($args)
+    {
+        $invoker = new TaskInvoker();
+        return $invoker->invokeCallable($this->sut, new TaskContext($this->runner->reveal(), new NullLogger(), $args));
+    }
+}
